0e14d1d61634132cc9ed17ba962bb75c790fb79e
Imashi Wimalasekera <wimalasekerakdisds.22@uom.lk>
2026-05-20 13:52:13 +0530

    Rename files

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import Layout from './components/layout/Layout'
 import LoginPage from './pages/LoginPage'
-import DashboardPage from './pages/DashboardPage'
-import ProductsPage from './pages/ProductsPage'
-import CategoriesPage from './pages/CategoriesPage'
-import OrdersPage from './pages/OrdersPage'
-import UsersPage from './pages/UsersPage'
-import PaymentsPage from './pages/PaymentsPage'
-import CouponsPage from './pages/CouponsPage'
-import InventoryPage from './pages/InventoryPage'
-import ReportsPage from './pages/ReportsPage'
-import NotificationsPage from './pages/NotificationsPage'
-import SettingsPage from './pages/SettingsPage'
+import AdminDashboard from './pages/AdminDashboard'
+import AdminProducts from './pages/AdminProducts'
+import AdminCategories from './pages/AdminCategories'
+import AdminOrders from './pages/AdminOrders'
+import AdminUsers from './pages/AdminUsers'
+import AdminPayments from './pages/AdminPayments'
+import AdminCoupons from './pages/AdminCoupons'
+import AdminInventory from './pages/AdminInventory'
+import AdminReports from './pages/AdminReports'
+import AdminNotifications from './pages/AdminNotifications'
+import AdminSettings from './pages/AdminSettings'
 
 const isAuthenticated = () => !!sessionStorage.getItem('token')
 
@@ -35,17 +35,17 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
           <Route index element={<Navigate to="/dashboard" replace />} />
-          <Route path="dashboard"     element={<DashboardPage />} />
-          <Route path="products"      element={<ProductsPage />} />
-          <Route path="categories"    element={<CategoriesPage />} />
-          <Route path="orders"        element={<OrdersPage />} />
-          <Route path="users"         element={<UsersPage />} />
-          <Route path="payments"      element={<PaymentsPage />} />
-          <Route path="coupons"       element={<CouponsPage />} />
-          <Route path="inventory"     element={<InventoryPage />} />
-          <Route path="reports"       element={<ReportsPage />} />
-          <Route path="notifications" element={<NotificationsPage />} />
-          <Route path="settings"      element={<SettingsPage />} />
+          <Route path="dashboard"     element={<AdminDashboard />} />
+          <Route path="products"      element={<AdminProducts />} />
+          <Route path="categories"    element={<AdminCategories />} />
+          <Route path="orders"        element={<AdminOrders />} />
+          <Route path="users"         element={<AdminUsers />} />
+          <Route path="payments"      element={<AdminPayments />} />
+          <Route path="coupons"       element={<AdminCoupons />} />
+          <Route path="inventory"     element={<AdminInventory />} />
+          <Route path="reports"       element={<AdminReports />} />
+          <Route path="notifications" element={<AdminNotifications />} />
+          <Route path="settings"      element={<AdminSettings />} />
         </Route>
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
